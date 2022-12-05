1eaa415d9c30b71e7bbfcafe69bff034a328c199
electronic <904103665@qq.com>
2022-12-06 02:57:00 +0800

    修改了二次

--- src/main/java/com/electronic/bean/Test.java
+++ src/main/java/com/electronic/bean/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("新建");
         System.out.println("修改了一次");
+        System.out.println("修改了二次");
     }
 }
